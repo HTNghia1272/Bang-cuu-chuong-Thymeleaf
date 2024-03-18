daab29517b139df3b5388ec051181f73b5d8c5b7
nghia.hoangtrong <hoangtrongnghia112@gmail.com>
2024-03-18 12:49:34 +0700

    Initital Commit

---
+++ demo10.5/src/main/java/vn/nghia/hoangtrong/demo105/rest/BangCuuChuongController.java
@@ -0,0 +1,16 @@
+package vn.nghia.hoangtrong.demo105.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class BangCuuChuongController {
+
+    @GetMapping("/hien-thi/{x}")
+    public  String hienThiBangCuuChuong(@PathVariable int x, Model model) {
+        model.addAttribute("number", x);
+        return "bang-cuu-chuong";
+    }
+}
